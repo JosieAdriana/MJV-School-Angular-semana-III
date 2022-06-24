2f585382c9587c1f6dd26274465ea8db9b5fe279
Josie Flôres <drijosie@gmail.com>
2022-06-24 16:52:51 -0300

    desenvolvimento do app

---
+++ src/app/features/employees/components/colaboradores/colaboradores.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colaboradores',
+  templateUrl: './colaboradores.component.html',
+  styleUrls: ['./colaboradores.component.scss']
+})
+export class ColaboradoresComponent implements OnInit {
+
+titulo = "Funcionários";
+
+  colaborador = {
+    id: 7,
+    nome: "Nathan Carlos Exercício",
+    salario: 4500,
+    cargo: "Desenvolvedor Pleno",
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("teste");
+  }
+saibaMais() {
+  alert ("Estamos ajustando para melhor atende-ló!");
+}
+}
